728d4afad941b9b3659bcea067cafe75b5c0633c
JuniorPastorini <juniorpastorini_@hotmail.com>
2023-10-25 07:13:47 -0300

    req 03

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,13 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if len(word) == 0:
+        return False
+
+    # Verificar se a palavra é um palíndromo
+    def check_palindrome(left, right):
+        if left >= right:
+            return True
+        if word[left] == word[right]:
+            return check_palindrome(left + 1, right - 1)
+        return False
+
+    return check_palindrome(0, len(word) - 1)
